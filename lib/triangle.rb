c97b8b5e256f27aca18040026ad2ed8fac042268
Kyler Best <kyler.b@live.com>
2022-11-28 09:53:09 -0600

    commit tree d8b0122668f2fa25b614be209d67d13fc85b2f70

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
 class Triangle
-  # write code here
+  attr_reader :a, :b, :c
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+  
+  def kind
+    validate
+    if a == b && b == c
+      return :equilateral
+    elsif a == b || b == c || c == a
+      return :isosceles
+    else
+      return :scalene
+    end
+  end
+
+  def validate
+    total = 0
+    [a, b, c].each{|side| total += side}
+    raise TriangleError unless [a, b, c].all?{|side| side > 0} && a + b > c && a + c > b && b + c > a
+  end
+
+  class TriangleError < StandardError
+
+  end
 end
